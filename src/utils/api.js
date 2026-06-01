a5e50065e1d373e722f6df4ed47e87b1a0bbb0d7
Valar Morghulis <amannice77@gmail.com>
2026-06-01 04:25:37 +0300

    Fix API URL to use env variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ems-backend-yeag.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
